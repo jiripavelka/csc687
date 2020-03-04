ce98728e3de65dc4cd0b9de3eb2bfc4e5bd6bc74
George <jiri.pavelka@internetguru.cz>
2020-03-03 23:16:38 -0500

    finish hw06

--- hw06.pl
+++ hw06.pl
@@ -23,13 +23,27 @@ for (my $i = 0; $i < @arr1; $i++) {
 my $rs = 1 - 6 * $di_ssum / (@arr1 * (@arr1**2 - 1));
 printf ("1. Spearman's rank correlation coefficient is %.3f.\n\n", $rs);
 
+print ("2.1 Output:\n\n");
 my %h;
+for (my $i = 100; $i <= 800; $i += 100) {
+    $h{$i} = [];
+}
+
+my @algo;
 open (IN, "CS_courses.txt");
 while (<IN>) {
     next if (index($_, ">CSC") < 0);
-    my $course = s/>CSC1/x/;
-    print $course."\n";
-    #print $l."\n";
-
+    $_ =~ /(CSC[^<#]+)/;
+    push (@{$h{substr($1, 3, 1)."00"}}, $1);
+    push (@algo, $1) if ($_ =~ /(CSC[\d\[\]]+) [^<]+<.+algorithm/i);
 }
 close (IN);
+
+foreach (sort keys %h) {
+    printf ("=== CSC%s ===\n", $_);
+    print join ("\n", @{$h{$_}});
+    print "\n\n";
+}
+
+printf ("2.2 Courses containing 'algorithm' in their introduction "
+    . "paragraph:\n%s\n\n", join (", ", @algo));
