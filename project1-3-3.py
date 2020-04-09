75e3340853a4a5c43a9bb90651930f4db62ff63b
George <jiri.pavelka@internetguru.cz>
2020-04-08 20:34:42 -0400

    finish prj 133 and the output

--- project1-3-3.py
+++ project1-3-3.py
@@ -6,13 +6,13 @@ coordFile = "chrX_1Mb_coordinates2.txt"
 
 with open(mapFile) as f:
   content = [line.split() for line in f]
-close(f)
+f.close()
 
 embedding = MDS(n_components=3)
 transformed = embedding.fit_transform(content)
 
 f = open(coordFile, "w")
 for line in transformed:
-  f.write(" ".join(line))
+  f.write(" ".join(str(x) for x in line))
   f.write("\n")
 f.close()
