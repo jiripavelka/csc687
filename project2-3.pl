96e1bd987cbbc38c9f38d3f8bec836882316d748
George <pavelka.iix@gmail.com>
2020-04-20 10:22:55 -0400

    generalize

--- project2-3.pl
+++ project2-3.pl
@@ -8,9 +8,11 @@ sub tap {
     my $ext = "$_[0]_$_[1]_$_[2]";
     my $mf = "models/RF_model_$ext";
     train ($_[0], $_[1], $mf);
-    my $ff = "x0$_[2]";
-    my $pf = "predictions/" . substr ($ff, 0, 8) . "_$ext";
-    predict ($mf, $ff, $pf) if (-f $ff);
+    return if not (-f $_[2]);
+    my $pf = "predictions/" . substr ($_[2], 0, 8) . "_$ext";
+    predict ($mf, $_[2], $pf);
+    my $ac = `cut -d" " -f1 < "$_[2]" | diff "$pf" - | grep ">" | wc -l` / `wc -l < "$_[2]"`;
+    print "$ext prediction accuracy = " . (100 - int ($ac * 1000) / 10) . " %\n";
 }
 
 sub train {
@@ -29,15 +31,15 @@ sub predict {
 }
 
 sub dolog {
-    die if not open (LOG, ">>", "perl.log");
+    die if not open (LOG, ">>", "p.log");
     print LOG localtime () . " " . $_[0] . "\n";
     close (LOG);
 }
 
-tap (100, 17, 0);
-tap (100, 24, 0);
-tap (200, 17, 0);
-tap (200, 24, 0);
+tap (100, 17, "full");
+tap (100, 24, "full");
+tap (200, 17, "full");
+tap (200, 24, "full");
 
 my $n = 5;
 my $l = int (`wc -l < "$TF"` / $n) + 1;
@@ -49,10 +51,10 @@ foreach my $i (1..$n) {
         next if $_ == $i;
         `cat "x0$_" >> "$TF"`;
     }
-    tap (100, 17, $i);
-    tap (100, 24, $i);
-    tap (200, 17, $i);
-    tap (200, 24, $i);
+    tap (100, 17, "x0$i");
+    tap (100, 24, "x0$i");
+    tap (200, 17, "x0$i");
+    tap (200, 24, "x0$i");
     `rm "$TF"`;
 }
 
